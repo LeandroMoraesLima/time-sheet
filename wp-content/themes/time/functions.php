0896d623e545c662787f8c6c17c5cf85430d39cd
leandro <le.mlima@hotmail.com>
2018-09-18 15:29:36 -0300

    foi feito o functions e a home

--- wp-content/themes/time/functions.php
+++ wp-content/themes/time/functions.php
@@ -7,20 +7,57 @@
 	function load_scripts()
 	{
 
+		$versao = '0.0.1';
 
 		/* Registering style */
-		wp_register_style('bootstrapcss', CSS . '/bootstrap.min.css', array(), '0.0.1', false );
+		wp_register_style('animatecss', CSS . '/animate.css', array(), $versao, false );
+		wp_register_style('bootstrapcss', CSS . '/bootstrap.css', array(), $versao, false );		
+		wp_register_style('entypo-fontellocss', CSS . '/entypo-fontello.css', array(), $versao, false );
+		wp_register_style('font-awesomecss', CSS . '/font-awesome.css', array(), $versao, false );
+		wp_register_style('main-mediacss', CSS . '/main-media.css', array(), $versao, false );
+		wp_register_style('maincss', CSS . '/main.css', array(), $versao, false );
+		wp_register_style('prettyphotocss', CSS . '/prettyPhoto.css', array(), $versao, false );
+		wp_register_style('skin_2css', CSS . '/skin_2.css', array(), $versao, false );
+
+
+
+		wp_register_script('bootstrapjs', JS . '/vendor/bootstrap.min.js', array('jquery'), $versao);
+		wp_register_script('conditionaljs', JS . '/vendor/conditional.min.js', array(''), $versao);
+		wp_register_script('jqueryjs', JS . '/vendor/jquery.fitvids.js', array(''), $versao);
+		wp_register_script('jqueryfl', JS . '/vendor/jquery.flexslider-min.js', array(''), $versao);
+		wp_register_script('jqueryow', JS . '/vendor/jquery.owlCarousel.js', array(''), $versao);
+		wp_register_script('jqueryst', JS . '/vendor/jquery.sticky.js', array(''), $versao);
+		wp_register_script('modernizrjs', JS . '/vendor/modernizr-2.6.1.min.js', array(''), $versao);
+		wp_register_script('smoothscrolljs', JS . '/vendor/SmoothScroll.min.js', array(''), $versao);
+		wp_register_script('mainjs', JS . '/main.js', array(''), $versao);
+		wp_register_script('pluginsjs', JS . '/plugins.js', array(''), $versao);
+
 		
-		wp_register_script( 'bootstrapjs', JS . '/bootstrap.min.js', array('jquery'), '0.0.1');
-				
 
 		wp_enqueue_style('bootstrapcss');
+		wp_enqueue_style('animatecss');
+		wp_enqueue_style('entypo-fontellocss');
+		wp_enqueue_style('font-awesomecss');
+		wp_enqueue_style('main-mediacss');
+		wp_enqueue_style('maincss');
+		wp_enqueue_style('prettyphotocss');
+		wp_enqueue_style('skin_2css');
 		
 
 		wp_enqueue_script('bootstrapjs');
+		wp_enqueue_script('conditionaljs');
+		wp_enqueue_script('jqueryjs');
+		wp_enqueue_script('jqueryfl');
+		wp_enqueue_script('jqueryow');
+		wp_enqueue_script('jqueryst');
+		wp_enqueue_script('modernizrjs');
+		wp_enqueue_script('smoothscrolljs');
+		wp_enqueue_script('mainjs');
+		wp_enqueue_script('pluginsjs');
 		
 		
 	}
 
+	add_action('wp_enqueue_scripts', 'load_scripts');
 	
 ?>
--- wp-content/themes/time/index.php
+++ wp-content/themes/time/index.php
@@ -2,9 +2,66 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>TimeShhet</title>
+
+	<?php wp_head(); ?>
+	
+	<link rel="stylesheet" href="<?php echo CSS; ?>/app.css?v=2">
+
 </head>
 <body>
-	
+
+	<header id="wt_header" class="wt_resp_nav_under_767 navbar navbar-fixed-top responsive_nav clearfix" role="banner" style="top: 0px;">    	
+		<div class="container">
+			<div id="logo" class="navbar-header">
+				<a class="navbar-brand" href="#">
+					<div class="slider" style="background-image: url('<?php echo IMG; ?>/logo.png')"></div>
+				</a>	
+			</div>		
+
+			<!-- Navigation -->
+			<nav id="nav" class="wt_nav_top collapse navbar-collapse" role="navigation" data-select-name="-- Main Menu --">      
+				<ul id="menu-menu-1" class="menu nav navbar-nav navbar-right">
+					<li id="menu-item-423" class="menu-item menu-item-type-custom menu-item-object-custom level-1-li firstItem current_page_item">
+						<a href="#" class="level-1-a">
+							<span data-hover="Home">Home</span>
+						</a>
+					</li>
+					<li id="menu-item-568" class="menu-item menu-item-type-post_type menu-item-object-page level-1-li">
+						<a href="#about" class="level-1-a">
+							<span data-hover="About">QUEM SOMOS</span>
+						</a>
+					</li>
+					<li id="menu-item-566" class="menu-item menu-item-type-post_type menu-item-object-page level-1-li">
+						<a href="#features" class="level-1-a">
+							<span data-hover="Features">PLANOS</span>
+						</a>
+					</li>
+					<li id="menu-item-569" class="menu-item menu-item-type-post_type menu-item-object-page level-1-li">
+						<a href="#faq" class="level-1-a">
+							<span data-hover="Faq">OUTROS PRODUTOS</span>
+						</a>
+					</li>
+					<li id="menu-item-570" class="menu-item menu-item-type-post_type menu-item-object-page level-1-li">
+						<a href="#pricing" class="level-1-a">
+							<span data-hover="Pricing">CANAL DE DENÚNCIA</span>
+						</a>
+					</li>
+					<li id="menu-item-571" class="menu-item menu-item-type-post_type menu-item-object-page level-1-li">
+						<a href="#new" class="level-1-a">
+							<span data-hover="News">CONTATO</span>
+						</a>
+					</li>
+					<li id="menu-item-564" class="menu-item menu-item-type-post_type menu-item-object-page level-1-li">
+						<a href="#clients" class="level-1-a">
+							<span data-hover="Clients">TRABALHE CONOSCO</span>
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</div> 	
+		<!-- End container -->    
+	</header>
+
 </body>
 </html>
