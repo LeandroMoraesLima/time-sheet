f14edc1819a7b78c9ab2a900bac7b814db54ccfc
leandro <le.mlima@hotmail.com>
2018-11-30 22:40:38 -0200

    foi alterado a pagina de pedidos

--- wp-content/themes/time/header.php
+++ wp-content/themes/time/header.php
@@ -7,7 +7,7 @@
 	<?php wp_head(); ?>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="<?php echo CSS; ?>/app.css?v=3">
+	<link rel="stylesheet" href="<?php echo CSS; ?>/app.css?v=4">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
